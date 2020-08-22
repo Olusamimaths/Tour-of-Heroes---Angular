855574c417096accb84032da193332acb9ad4d8d
Samuel Olusola <olusola.samuel.oluwatobi@gmail.com>
2020-08-22 15:44:14 +0100

    ✨ Error handling in HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,17 +3,44 @@ import { Hero } from './heroes/hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) {}
+
+  private heroesUrl = 'api/heroes';
+
+  private log(message: string): void {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
 
   getHeroes(): Observable<Hero[]> {
     // send a message when heroes are fetched
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES); // returns an Observable<Hero[]> that emits a single value, the array of mock heroes
+    this.log('HeroService: fetched heroes');
+    return this.http
+      .get<Hero[]>(this.heroesUrl)
+      .pipe(catchError(this.handleError<Hero[]>('getHeroes', [])));
   }
 
   getHero(id: number): Observable<Hero> {
